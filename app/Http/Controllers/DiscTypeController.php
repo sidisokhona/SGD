7ddd8a67798abb8f67b5da73b0437803a8a962bd
Sidi SOKHONA <sokhonasidi@yahoo.fr>
2021-03-25 12:24:02 +0100

    Création du Controleur DiscTypeController

---
+++ app/Http/Controllers/DiscTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DiscType;
+use Illuminate\Http\Request;
+
+class DiscTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DiscType  $discType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DiscType $discType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DiscType  $discType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DiscType $discType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DiscType  $discType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DiscType $discType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DiscType  $discType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DiscType $discType)
+    {
+        //
+    }
+}
