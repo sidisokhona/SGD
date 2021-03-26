b17dd78de931ef983a6aa2d7de14b26f10164858
Sidi SOKHONA <sokhonasidi@yahoo.fr>
2021-03-26 18:23:43 +0100

    les importation de mes composants dans app.js et et la déclaration des routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,29 +4,43 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+ require('./bootstrap');
 
-window.Vue = require('vue').default;
+ window.Vue = require('vue').default;
+ 
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+ 
+Vue.component('pagination', require('laravel-vue-pagination'));
+ 
+ Vue.component('add-ray', require('./components/raysComponent/addRayComponent.vue').default);
+ Vue.component('edit-ray', require('./components/raysComponent/editRayComponent.vue').default);
+ 
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+ 
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+ import Ray from './components/raysComponent/raysListeComponent.vue';
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+ 
+ 
+ 
+ const routes = [
+     {
+         path: '/Rays',
+         component:Ray
+ 
+     }
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
-});
+ ];
+ 
+ const router = new VueRouter({routes});
+ 
+ 
+ 
+ 
+ 
+ const app = new Vue({
+     el: '#app',
+     router:router
+ });
+ 
