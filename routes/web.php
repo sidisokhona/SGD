d91161b4e87184d734c8e8f9f05b1ac80739d2ac
Sidi SOKHONA <sokhonasidi@yahoo.fr>
2021-03-26 18:25:46 +0100

    Définition des routes dans web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
-
+use App\Http\Controllers\RayController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+
+// --------------------------------------------------------------------
+// -------------------------Ray Route------------------------------
+Route::get('/rays', [RayController::class, 'index']);
+Route::post('/rays', [RayController::class, 'store']);
+Route::get('/rays/edit/{id}', [RayController::class, 'edit']);
+Route::patch('/rays/edit/{id}', [RayController::class,'update']);
+Route::delete('/rays/{id}', [RayController::class,'destroy']);
+// --------------------------------------------------------------------
+// -------------------------End Ray Route------------------------------
+
+
 
 Route::get('/', function () {
     return view('welcome');
